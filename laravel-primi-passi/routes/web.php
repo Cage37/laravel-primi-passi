40a7d92e09b2b659eb2ba66bae99af11c3d410c4
Cage37 <casiraghi.smn@gmail.com>
2021-07-07 16:49:23 +0200

    Task complete

---
+++ laravel-primi-passi/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $welcome = "HELLO LARAVEL";
+
+    $sentences = [
+                     "I Love Laravel",
+                    "Laravel is Fantstic",
+                   "Well done Laravel!"
+    ];
+
+    $end = "THE END... FOR TODAY!";
+
+    return view('home', compact("welcome", "end", "sentences"));
+});
